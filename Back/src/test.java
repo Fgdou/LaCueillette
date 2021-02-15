a2d62bd2c46fba2bedb8ec3bd3860806850af3b4
fgdou <fabigoardou@gmail.com>
2021-02-15 16:33:59 +0100

    Change database

--- Back/src/test.java
+++ Back/src/test.java
@@ -1,5 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 class test{
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Starting main service...");
+
+        Connection cn = null;
+        Statement st = null;
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection("localhost", "root", "g7Nn5DkEBLCbpCTNw84FPkw3wjoDPYu4KJ2NSSkb");
+            st = cn.createStatement();
+            String sql = "INSERT INTO test (id) VALUES (3)";
+            st.executeUpdate(sql);
+        }catch(Exception e){
+
+        }
     }
 }
