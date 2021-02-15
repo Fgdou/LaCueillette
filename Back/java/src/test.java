9543f03976a5be79f9c16e7c2ac9c77985260a3c
fgdou <fabigoardou@gmail.com>
2021-02-15 22:13:32 +0100

    Setup mysql Setup jdbc

--- Back/java/src/test.java
+++ Back/java/src/test.java
@@ -10,13 +10,12 @@ class test{
         Statement st = null;
 
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            cn = DriverManager.getConnection("localhost", "root", "g7Nn5DkEBLCbpCTNw84FPkw3wjoDPYu4KJ2NSSkb");
+            cn = DriverManager.getConnection("jdbc:mysql://localhost/LaCueillette", "root", "g7Nn5DkEBLCbpCTNw84FPkw3wjoDPYu4KJ2NSSkb");
             st = cn.createStatement();
             String sql = "INSERT INTO test (id) VALUES (3)";
             st.executeUpdate(sql);
         }catch(Exception e){
-
+            throw new RuntimeException(e);
         }
     }
 }
