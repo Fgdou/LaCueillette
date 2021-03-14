099ab8e4592e6b9b5270f63f224a57cb60693395
thibaut <thibaut.raoul@etudiant.univ-rennes1>
2021-03-14 19:04:14 +0100

    Add new classes and updates to UserController.java
    Add methods like login, logout and request 'token' when needed.

---
+++ Back/java/src/serveur/restservice/OrderController.java
@@ -0,0 +1,11 @@
+package serveur.restservice;
+
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+import serveur.sql.User;
+
+import java.util.Map;
+
+@RestController
+public class OrderController {
+}
