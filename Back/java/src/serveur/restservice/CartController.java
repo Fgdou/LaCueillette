c122df38f9e27dad1d448ed25c216ba8f0cc781a
thibaut <thibaut.raoul@etudiant.univ-rennes1>
2021-03-17 21:55:01 +0100

    Add methods to StoreController.java and OrderController.java
    Add methods in the road of 100% !

--- Back/java/src/serveur/restservice/CartController.java
+++ Back/java/src/serveur/restservice/CartController.java
@@ -2,10 +2,73 @@ package serveur.restservice;
 
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
+import serveur.sql.Cart;
+import serveur.sql.Store;
 import serveur.sql.User;
 
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class CartController {
+
+    /**
+     * Add a product to user's cart
+     *
+     * @param requestParam Parameters requested : user_token, product_id, quantity (g or unity)
+     * @return The cart
+     * @throws Exception
+     */
+    @PostMapping("/cart/add")
+    public Cart addProduct(@RequestParam Map<String, String> requestParam) throws Exception {
+        String token = requestParam.get("user_token");
+        User user = User.getByToken(token);
+
+        int product_id = Integer.parseInt(requestParam.get("product_id"));
+        int quantity = Integer.parseInt(requestParam.get("quantity"));
+
+        //TODO setCart(product, quantity)
+        return null;
+    }
+
+    /**
+     * Modify or delete a product in user's cart
+     *
+     * @param requestParam Parameters requested : user_token, product_id, quantity (g or unity)
+     * @return The cart
+     * @throws Exception
+     */
+    @PostMapping("/cart/modify")
+    public Cart modifyProduct(@RequestParam Map<String, String> requestParam) throws Exception {
+        String token = requestParam.get("user_token");
+        User user = User.getByToken(token);
+
+        int product_id = Integer.parseInt(requestParam.get("product_id"));
+        int quantity = Integer.parseInt(requestParam.get("quantity"));
+
+        //if (quantity == 0)
+        //On supprime l'article
+        //else
+        //On modifie juste la quantité
+
+        //TODO setCart -> même chose
+        return null;
+    }
+
+    /**
+     * Validate a cart : create order for each store
+     *
+     * @param requestParam Parameters requested : user_token
+     * @return List of the order_id generated
+     * @throws Exception
+     */
+    public List<Integer> validateCart(@RequestParam Map<String, String> requestParam) throws Exception {
+        User user = User.getByToken(requestParam.get("user_token"));
+
+        Cart cart = Cart.getByUser(user);
+
+        //Pour chaque produit, assigner à un store puis créer les commandes en fonction des assignations
+
+        return new ArrayList<>();
+    }
+
 }
--- Back/java/src/serveur/restservice/OrderController.java
+++ Back/java/src/serveur/restservice/OrderController.java
@@ -1,11 +1,114 @@
 package serveur.restservice;
 
-import org.json.JSONObject;
+import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.*;
+import serveur.sql.Order;
+import serveur.sql.Product;
+import serveur.sql.Store;
 import serveur.sql.User;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class OrderController {
+
+    /**
+     * Get a list of order liked with a store
+     *
+     * @param requestParam Parameters required : user_token, store_id
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/order/get/byStore")
+    public List<Order> getOrderByStore(@RequestParam Map<String, String> requestParam) throws Exception {
+        User user = User.getByToken(requestParam.get("user_token"));
+        Store store = Store.getById(Integer.parseInt(requestParam.get("store_id")));
+
+        if (!store.getSeller().equals(user) || !user.isAdmin())
+            throw new Exception("You are not the owner of this store or you are not admin");
+
+        return store.getOrders();
+    }
+
+    /**
+     * Get a list of products in an order
+     *
+     * @param requestParam Parameters required : user_token, order_id
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/order/get/products")
+    public Map<Integer, Integer> getProductsInOrder(@RequestParam Map<String, String> requestParam) throws Exception {
+        User user = User.getByToken(requestParam.get("user_token"));
+        Order order = Order.getById(Integer.parseInt(requestParam.get("order_id")));
+        Store store = order.getStore();
+
+        if ((!store.getSeller().equals(user) && !order.getUser().equals(user)) || !user.isAdmin())
+            throw new Exception("You are not the owner of this store, you are not the buyer or you are not admin");
+
+        //TODO getProductsInOrder
+
+        return null;
+    }
+
+    /**
+     * Get the store who prepares the order
+     *
+     * @param requestParam Parameters required : user_token, order_id
+     * @return Store who prepares the order
+     * @throws Exception
+     */
+    @GetMapping("/order/get/store")
+    public Store getStore(@RequestParam Map<String, String> requestParam) throws Exception {
+        User user = User.getByToken(requestParam.get("user_token"));
+        Order order = Order.getById(Integer.parseInt(requestParam.get("order_id")));
+        Store store = order.getStore();
+
+        if ((!store.getSeller().equals(user) && !order.getUser().equals(user)) || !user.isAdmin())
+            throw new Exception("You are not the owner of this store, you are not the buyer or you are not admin");
+
+        return store;
+    }
+
+    /**
+     * Get the order's buyer
+     *
+     * @param requestParam Parameters required : user_token, order_id
+     * @return The user who buys
+     * @throws Exception
+     */
+    @GetMapping("/order/get/buyer")
+    public User getBuyer(@RequestParam Map<String, String> requestParam) throws Exception {
+        User user = User.getByToken(requestParam.get("user_token"));
+        Order order = Order.getById(Integer.parseInt(requestParam.get("order_id")));
+        Store store = order.getStore();
+
+        if ((!store.getSeller().equals(user) && !order.getUser().equals(user)) || !user.isAdmin())
+            throw new Exception("You are not the owner of this store, you are not the buyer or you are not admin");
+
+        return order.getUser();
+    }
+
+    /**
+     * Get PriceHT and TVA of an order
+     * @param requestParam Parameters required : user_token, order_id
+     * @return Pair of the priceHT and priceTTC
+     * @throws Exception
+     */
+    public Pair<Float, Float> getPriceAndTVA(@RequestParam Map<String, String> requestParam) throws Exception {
+        User user = User.getByToken(requestParam.get("user_token"));
+        Order order = Order.getById(Integer.parseInt(requestParam.get("order_id")));
+        Store store = order.getStore();
+
+        if ((!store.getSeller().equals(user) && !order.getUser().equals(user)) || !user.isAdmin())
+            throw new Exception("You are not the owner of this store, you are not the buyer or you are not admin");
+
+        return Pair.of(order.getPriceHT(), order.getPriceTTC());
+    }
+
+    //TODO Set Paid
+
+
+
 }
