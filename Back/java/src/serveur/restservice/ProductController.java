e82bcde63891c64c16131fb4ad5e92a6acb22ed5
thibaut <thibaut.raoul@etudiant.univ-rennes1>
2021-03-17 15:58:49 +0100

    Add methods to ProductController.java
    Add methods for the API. In need of information about the dates in Product's class.

--- Back/java/src/serveur/restservice/ProductController.java
+++ Back/java/src/serveur/restservice/ProductController.java
@@ -2,12 +2,182 @@ package serveur.restservice;
 
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
+import serveur.DateTime;
+import serveur.sql.Product;
+import serveur.sql.Store;
 import serveur.sql.User;
 
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ProductController {
+
+    /**
+     * Create a new product in the DataBase
+     * @param requestParam Parameters requested : token, name, price, price_kg, category_id, store_id, canBePicked, canBeDelivered, tva, time_start, time_stop, expiration, description
+     * @return JSONObject : error or log with the id
+     * @throws Exception
+     */
+    @PutMapping("/product/new")
+    public JSONObject addProduct(@RequestParam Map<String, String> requestParam) throws Exception{
+        String token = requestParam.get("token");
+        int store_id = Integer.parseInt(requestParam.get("store_id"));
+        String name = requestParam.get("name");
+        int quantity = Integer.parseInt(requestParam.get("quantity"));
+        double price = Double.parseDouble(requestParam.get("price"));
+        double tva = Double.parseDouble(requestParam.get("tva"));
+        String description = requestParam.get("description");
+        boolean price_kg = requestParam.get("price_kg").toLowerCase().equals("true") ? true : false;
+        boolean canBeDelivered = requestParam.get("canBeDelivered").toLowerCase().equals("true") ? true : false;
+        boolean canBePicked = requestParam.get("canBePicked").toLowerCase().equals("true") ? true : false;
+        //TODO Date1, Date2, Expiration
+        try{
+            User user = User.getByToken(token);
+            Store store = Store.getById(store_id);
+
+            if(!store.getSeller().equals(user))
+                throw new Exception("You are not the owner of this store");
+
+            Product product = Product.create(name, (float)price, price_kg, null, store, canBePicked, canBeDelivered, (float)tva, null, null, null, description);
+            return new JSONObject().put("log", "product created: " + product.getId());
+        } catch (Exception e){
+            return new JSONObject().put("error", e.getMessage());
+        }
+    }
+
+    /**
+     * Modify a product in the DataBase
+     * @param requestParam Parameters requested : token, product_id, name, price, price_kg, category_id, store_id, canBePicked, canBeDelivered, tva, time_start, time_stop, expiration, description
+     * @return JSONObject : error or log with the id
+     * @throws Exception
+     */
+    @PostMapping("/product/modify")
+    public JSONObject modifyProduct(@RequestParam Map<String, String> requestParam) throws Exception{
+        String token = requestParam.get("token");
+        int store_id = Integer.parseInt(requestParam.get("store_id"));
+        int product_id = Integer.parseInt(requestParam.get("product_id"));
+        String name = requestParam.get("name");
+        double price = Double.parseDouble(requestParam.get("price"));
+        double tva = Double.parseDouble(requestParam.get("tva"));
+        String description = requestParam.get("description");
+        boolean price_kg = requestParam.get("price_kg").toLowerCase().equals("true") ? true : false;
+        boolean canBeDelivered = requestParam.get("canBeDelivered").toLowerCase().equals("true") ? true : false;
+        boolean canBePicked = requestParam.get("canBePicked").toLowerCase().equals("true") ? true : false;
+        //TODO Date1, Date2, Expiration
+        try{
+            User user = User.getByToken(token);
+            Store store = Store.getById(store_id);
+
+            if(!store.getSeller().equals(user))
+                throw new Exception("You are not the owner of this store");
+
+            Product product = Product.getById(product_id);
+
+            if(!name.equals(""))
+                product.setName(name);
+            product.setPrice((float) price);
+            product.setTva((float) tva);
+            if(!description.equals(""))
+                product.setDescription(description);
+            product.setPrice_kg(price_kg);
+            product.setCanBeDelivered(canBeDelivered);
+            product.setCanBePicked(canBePicked);
+            return new JSONObject().put("log", "product modified: " + product.getId());
+        } catch (Exception e){
+            return new JSONObject().put("error", e.getMessage());
+        }
+    }
+
+    /**
+     * Delete a product from database
+     * @param requestParam Parameters required : token, store_id, product_id
+     * @return JSONObject : error or log
+     * @throws Exception
+     */
+    @PostMapping("/product/delete")
+    public JSONObject deleteProduct(@RequestParam Map<String, String> requestParam) throws Exception{
+        String token = requestParam.get("token");
+        int store_id = Integer.parseInt(requestParam.get("store_id"));
+        int product_id = Integer.parseInt(requestParam.get("product_id"));
+        try{
+            User user = User.getByToken(token);
+            Store store = Store.getById(store_id);
+
+            if(!store.getSeller().equals(user))
+                throw new Exception("You are not the owner of this store");
+
+            Product product = Product.getById(product_id);
+            product.delete();
+            return new JSONObject().put("log", "product deleted");
+        } catch (Exception e){
+            return new JSONObject().put("error", e.getMessage());
+        }
+    }
+
+    /**
+     * Getting the quantity available of a ptoduct
+     * @param requestParam Parameters required : token, id
+     * @return JSONObject : error or log + the quantity available of a ptoduct
+     * @throws Exception
+     */
+    @GetMapping("/product/get/quantityAvailable")
+    public JSONObject getQuantityAvailable(@RequestParam Map<String, String> requestParam) throws Exception{
+        String token = requestParam.get("token");
+        int id = Integer.parseInt(requestParam.get("id"));
+        try{
+            User user = User.getByToken(token);
+            Product product = Product.getById(id);
+            return new JSONObject().put("log", "quantity: " + 0);
+            //TODO Product.getQuantity()
+        }catch (Exception e){
+            return new JSONObject().put("error", e.getMessage());
+        }
+    }
+
+    /**
+     * Getting every product for a given store
+     * @param requestParam Parameters requested : store_id
+     * @return JSONObject : error or log + list
+     * @throws Exception
+     */
+    @GetMapping("/product/get/byStore")
+    public JSONObject getProductByStore(@RequestParam Map<String, String> requestParam) throws Exception{
+        int store_id = Integer.parseInt(requestParam.get("store_id"));
+        JSONObject returnJson = new JSONObject();
+        try{
+            Store store = Store.getById(store_id);
+            //TODO Liste des id de produits d'un store
+        } catch (Exception e){
+            returnJson.put("error", e.getMessage());
+        }
+        return returnJson;
+    }
+
+    /**
+     * Get infos about a product
+     * @param requestParam Parameter requested : product_id
+     * @return JSONObject : error or log + product
+     * @throws Exception
+     */
+    @GetMapping("/product/get/infos")
+    public JSONObject getProductInfos(@RequestParam Map<String, String> requestParam) throws Exception{
+        int product_id = Integer.parseInt(requestParam.get("product_id"));
+        JSONObject jsonObject = new JSONObject();
+        try{
+            Product product = Product.getById(product_id);
+            jsonObject.put("log", "infos about: " + product_id);
+            jsonObject.put("name", product.getName());
+            jsonObject.put("price", product.getPrice());
+            jsonObject.put("price_kg", product.isPrice_kg());
+            jsonObject.put("canBeDelivered", product.canBeDelivered());
+            jsonObject.put("canBePicked", product.canBePicked());
+            jsonObject.put("expiration", product.getExpiration());
+            jsonObject.put("description", product.getDescription());
+        } catch (Exception e){
+            jsonObject.put("error", e.getMessage());
+        }
+        return jsonObject;
+    }
+
 
 }
