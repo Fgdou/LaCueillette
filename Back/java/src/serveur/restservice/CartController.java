cd3b9df7d1aef13ade7ecf2e95e134c08e6cf8b8
thibaut <thibaut.raoul@etudiant.univ-rennes1>
2021-03-18 14:53:12 +0100

    Add updates to CartController.java

--- Back/java/src/serveur/restservice/CartController.java
+++ Back/java/src/serveur/restservice/CartController.java
@@ -1,11 +1,7 @@
 package serveur.restservice;
 
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
-import serveur.sql.Cart;
-import serveur.sql.Order;
-import serveur.sql.Store;
-import serveur.sql.User;
+import serveur.sql.*;
 
 import java.util.*;
 
@@ -27,7 +23,10 @@ public class CartController {
         int subproduct_id = Integer.parseInt(requestParam.get("subproduct_id"));
         int quantity = Integer.parseInt(requestParam.get("quantity"));
 
-        return Cart.getByUser(user).addProduct(subproduct_id, quantity);
+        Cart cart = Cart.getByUser(user);
+        cart.addProduct(SubProduct.getById(subproduct_id), quantity);
+
+        return cart;
     }
 
     /**
@@ -47,9 +46,9 @@ public class CartController {
         int quantity = Integer.parseInt(requestParam.get("quantity"));
 
         if (quantity == 0)
-            cart.removeProduct(subproduct_id);
+            cart.removeProduct(SubProduct.getById(subproduct_id));
         else
-            cart.changeQuantity(subproduct_id, quantity);
+            cart.changeQuantity(SubProduct.getById(subproduct_id), quantity);
 
         return cart;
     }
@@ -57,15 +56,19 @@ public class CartController {
     /**
      * Validate a cart : create order for each store
      *
-     * @param requestParam Parameters requested : user_token
+     * @param requestParam Parameters requested : user_token, address_id
      * @return List of the order generated
      * @throws Exception
      */
     public List<Order> validateCart(@RequestParam Map<String, String> requestParam) throws Exception {
         User user = User.getByToken(requestParam.get("user_token"));
         Cart cart = Cart.getByUser(user);
+        Address address = Address.getById(Integer.parseInt(requestParam.get("address_id")));
 
-        return cart.buy();;
+        if (!address.getUser().equals(user))
+            throw new Exception("The address not belong to the user");
+
+        return cart.buy(address);
     }
 
 }
